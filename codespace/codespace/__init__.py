342ce5052a27bac1effc1788ab5f292463263ab4
Farhan Nurdiatama Pakaya <farhan.np9@gmail.com>
2023-02-13 08:14:36 +0000

    feature: add sample endpoint using aiohttp

--- codespace/codespace/__init__.py
+++ codespace/codespace/__init__.py
@@ -1,3 +1,14 @@
+from aiohttp import web
+
+async def handle(request):
+    name = request.match_info.get('name', "World")
+    text = "Hello, " + name
+    return web.Response(text=text)
+
+
+app = web.Application()
+app.add_routes([web.get('/', handle),
+                web.get('/{name}', handle)])
 
 def main():
-    print('hello remote world')
+    web.run_app(app, port=8080)
